ac9743bdcf96184399ced1a961e6946d304d7354
Rudi von Staden <rudivs@gmail.com>
2019-09-05 20:28:35 +0200

    Added Recipe model

---
+++ RecipeManager/Models/Recipe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace RecipeManager.Models
+{
+    public class Recipe
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "title")]
+        public string Title { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "recipeSteps")]
+        public List<string> RecipeSteps { get; set; }
+
+        [JsonProperty(PropertyName = "notes")]
+        public string Notes { get; set; }
+
+        [JsonProperty(PropertyName = "ownerId")]
+        public string OwnerId { get; set; }
+    }
+}
