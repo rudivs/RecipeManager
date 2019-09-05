466855e1ee7f63052f66960582faa6017ee87c19
Rudi von Staden <rudivs@gmail.com>
2019-09-05 20:02:54 +0200

    Enable MVC, setup default route

---
+++ RecipeManager/Controllers/RecipeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeManager.Controllers
+{
+    public class RecipeController : Controller
+    {
+        [ActionName("Index")]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
--- RecipeManager/Startup.cs
+++ RecipeManager/Startup.cs
@@ -15,6 +15,7 @@ namespace RecipeManager
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +26,13 @@ namespace RecipeManager
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute("default",
+                    "{controller=Recipe}/{action=Index}/{id?}");
             });
         }
     }
