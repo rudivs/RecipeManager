301e39f8e6ec271f5452f10ece6ea0ae2c875aca
Rudi von Staden <rudivs@gmail.com>
2019-09-11 22:23:25 +0200

    Basic scaffolding for Angular

---
+++ RecipeManager/wwwroot/clientapp/out-tsc/app/app.component.js
@@ -0,0 +1,23 @@
+import * as tslib_1 from "tslib";
+import { Component } from '@angular/core';
+let AppComponent = class AppComponent {
+    constructor() {
+        this.title = 'Recipe Manager';
+    }
+};
+AppComponent = tslib_1.__decorate([
+    Component({
+        selector: 'app-root',
+        template: `
+    <div style="text-align:center" class="content">
+      <h1>
+        {{title}}
+      </h1>
+     </div>
+    <router-outlet></router-outlet>
+  `,
+        styles: []
+    })
+], AppComponent);
+export { AppComponent };
+//# sourceMappingURL=app.component.js.map
